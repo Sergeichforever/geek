918ba22bb953380498ea3907d4a4f704beb9895b
sergeich <sergeichforever@inbox.ru>
2019-01-20 14:59:08 +0300

    Revert "correct pr001.c"
    This reverts commit 0712167f4269b630785acf52fe6fa1e1d7e9c147.

--- pr001.c
+++ pr001.c
@@ -3,9 +3,13 @@
 int main()
 {
         int i;
-        for (i=1; i<6; i++) 
+        for (i=1; i<5; i++) 
         {
+<<<<<<< HEAD
          puts("\tHello words ");
+=======
+         puts("Hello words \n");
+>>>>>>> parent of 0712167... correct pr001.c
         }
         return 0;
 }
